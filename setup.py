7ebe4c819569c3abfa9b6687cbf6b7f12e2863b0
DaanJansen94 <jansendaan94@gmail.com>
2026-01-27 10:53:21 +0100

    Rename module to virasign.py and update entrypoints

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "virasign=virasign.reference_selection:main",
+            "virasign=virasign.virasign:main",
         ],
     },
     classifiers=[
