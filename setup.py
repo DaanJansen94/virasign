fc5f0fc069abc791d32090dfc86222a38b39d4c6
DaanJansen94 <jansendaan94@gmail.com>
2026-04-12 21:29:27 +0200

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="virasign",
-    version="0.0.2",
+    version="0.0.3",
     author="Virasign Team",
     description="Virasign: Viral Read ASSIGNment from nanopore sequencing",
     long_description=long_description,
