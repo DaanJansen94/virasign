f1a4d7fc23e9d9dde93d078506d3f3092dd9d0f8
DaanJansen94 <jansendaan94@gmail.com>
2026-04-13 14:53:02 +0200

    .

--- virasign/__init__.py
+++ virasign/__init__.py
@@ -1,6 +1,6 @@
 """Virasign: Viral Read ASSIGNment from nanopore sequencing."""
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 # Import main function from virasign.py in the same directory
 from .virasign import main
